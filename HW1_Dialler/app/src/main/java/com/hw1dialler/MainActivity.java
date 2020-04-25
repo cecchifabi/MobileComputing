9ec8f53f2363698156d1488099a878c4ee33f8be
cecchifabi <cecchifabi@gmail.com>
2020-04-25 11:37:16 +0100

    Dialler (with SharedPreferences)

--- HW1_Dialler/app/src/main/java/com/hw1dialler/MainActivity.java
+++ HW1_Dialler/app/src/main/java/com/hw1dialler/MainActivity.java
@@ -3,9 +3,11 @@ package com.hw1dialler;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     public static final int TEXT_REQUEST = 1;
     public static final String EXTRA_ID = "com.hw1dialler.extra.ID";
 
+    SharedPreferences sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +37,19 @@ public class MainActivity extends AppCompatActivity {
 
         phoneNumber = (EditText)findViewById(R.id.number);
 
+        sharedPref = this.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+
+        String SD1 = sharedPref.getString(getString(R.string.sd1), "SD1 null");
+        String SD2 = sharedPref.getString(getString(R.string.sd2), "SD2 null");
+        String SD3 = sharedPref.getString(getString(R.string.sd3), "SD3 null");
+
+        Log.d("MainActivity", SD1);
+        Log.d("MainActivity", SD2);
+        Log.d("MainActivity", SD3);
+
         sd1 = (Button)findViewById(R.id.sd_1);
+        sd1.setText(SD1.split(" ")[0]);
+        sdNumber1 = SD1.split(" ")[1];
         sd1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -49,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         sd2 = (Button)findViewById(R.id.sd_2);
+        sd2.setText(SD2.split(" ")[0]);
+        sdNumber2 = SD2.split(" ")[1];
         sd2.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -64,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
         });
 
         sd3 = (Button)findViewById(R.id.sd_3);
+        sd3.setText(SD3.split(" ")[0]);
+        sdNumber3 = SD3.split(" ")[1];
         sd3.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
@@ -96,10 +116,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + n));
         if (intent.resolveActivity(getPackageManager()) != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                // Permissions not granted: inform the user
-                Toast.makeText(getApplicationContext(), "Permissions to call not granted", Toast.LENGTH_SHORT).show();
-                return;
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // Ask the user to allow phone calls
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 0);
             }
             startActivity(intent);
         }
@@ -143,9 +162,8 @@ public class MainActivity extends AppCompatActivity {
         intent.setData(Uri.parse("tel:" + number));
         if (intent.resolveActivity(getPackageManager()) != null) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                // Permissions not granted: inform the user
-                Toast.makeText(getApplicationContext(), "Permissions to call not granted", Toast.LENGTH_SHORT).show();
-                return;
+                // Ask the user to allow phone calls
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, 0);
             }
             startActivity(intent);
         }
@@ -171,5 +189,10 @@ public class MainActivity extends AppCompatActivity {
             sd3.setText(name);
             sdNumber3 = number;
         }
+
+        // Write on SharedPreferences
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(id, name + " " + number);
+        editor.commit();
     }
 }
